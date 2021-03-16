f249a315f70e6cc954141073a1edf5304b4d3a14
jason stark <35188849+jiangwenfan@users.noreply.github.com>
2021-03-16 17:55:37 +0800

    test
    123

--- c7AutoVpsConfig.sh
+++ c7AutoVpsConfig.sh
@@ -11,3 +11,12 @@ echo "download frp..."
 wget https://github.com/fatedier/frp/releases/download/v0.35.1/frp_0.35.1_linux_amd64.tar.gz
 tar -zxvf frp_0.35.1_linux_amd64.tar.gz -C /opt/
 
+echo "install python3...."
+wget https://raw.githubusercontent.com/jiangwenfan/linuxManagementScript/main/c7AutoPy3.sh
+chmod +x c7AutoPy3.sh
+./c7AutoPy3.sh
+
+echo "install java8...."
+wget https://github.com/jiangwenfan/linuxManagementScript/blob/main/c7AutoInstallJDK8.sh
+chmod +x c7AutoInstallJDK8.sh
+./c7AutoInstallJDK8.sh
