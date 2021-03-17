1ded785dba30c2d91c822d57d7eed6b787e37a84
jason stark <35188849+jiangwenfan@users.noreply.github.com>
2021-03-17 19:52:58 +0800

    test
    12

--- c7AutoInstallJDK8.sh
+++ c7AutoInstallJDK8.sh
@@ -6,13 +6,18 @@ echo "
 1.本地获取
 2.云存储获取(暂时未开启)
 "
+if [ ! -f "jdk-8u281-linux-x64.tar.gz" ]
+then
+    wget http://sick.pwall.icu:62022/jdk-8u281-linux-x64.tar.gz
+fi
+
 if [ ! -d "/opt/module" ]
 then
     mkdir /opt/module #module不存在，创建。
 fi
 echo "解压配置"
-tar -zxvf xxx.tar.gz -C /opt/module
-ln -s java----- jdk
+tar -zxvf jdk-8u281-linux-x64.tar.gz -C /opt/module
+ln -s jdk-8u281-linux-x64 jdk
 echo "
 JAVA_HOME=/opt/module/jdk
 CLASS_HOME=$JAVA_HOME/lib
