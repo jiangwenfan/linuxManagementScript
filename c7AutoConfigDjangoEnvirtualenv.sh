3eeedc7bbd6bf04c6eb0186d45dcd727ad57522b
Your Name <you@example.com>
2021-05-05 22:03:47 +0800

    modify

--- c7AutoConfigDjangoEnvirtualenv.sh
+++ c7AutoConfigDjangoEnvirtualenv.sh
@@ -14,7 +14,7 @@ source ~/.bashrc
 #创建虚拟环境 
 #.virtualenvs/test1 虚拟环境的具体存放路径 
 #rmvirtualenv t1 #删除虚拟环境 
-#work #查看所有虚拟环境 
+#workon #查看所有虚拟环境 
 #deactivate #退出虚拟环境 
 #workon test1 #进入虚拟环境
 #pip list pip freeze #查看已安装的包 pip install django==1.8.2
